4e013b0868fb83add85eae8d70935b724fc9204c
Gautam Sinha <gautam.sinha@live.com>
2022-04-05 13:14:52 -0400

    created fighter screen

--- src/App.js
+++ src/App.js
@@ -10,8 +10,13 @@ function App() {
       <div className="fighters-grid">
       <Fighter name={'Roy'} color={'blue'} />
       <Fighter name={'Fox'} color={'green'} />
-      <Fighter name={'Ganondorf'} color={'purple'} />
-      
+      <Fighter name={'Ganondorf'} color={'purple'}/>
+      <Fighter name={'Sheik'} color={'lightskyblue'}/>
+      </div>
+      <div className="fighter-screen"> 
+        <h2>Roy</h2>
+        <img src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png"/> 
+        <p>lorem ipsum</p>
       </div>
     </div>
   );

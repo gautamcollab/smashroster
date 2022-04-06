aafa32f69b610d80670d40e7070877658baff726
Gautam Sinha <gautam.sinha@live.com>
2022-04-06 10:52:07 -0400

    added chars

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ const characters = [
   {name: 'Roy', color: 'red'},
   {name: 'Fox', color: 'green'}, 
   {name: 'Ganondorf', color: 'purple'}, 
-  {name: 'Sheik', color: 'lightskyblue'}
+  {name: 'Sheik', color: 'lightskyblue'}, 
+  {name: 'Cloud', color: 'mediumseagreen'}, 
+  {name: 'Falco', color: 'orange'},
+  {name: 'Samus', color: 'red'},
+  {name: 'Link', color: 'yellow'},
 ]
 
 
 function App() {
   const [visible, setVisible] = useState(false)
+  const [selectedCharacter, setSelectedCharacter] = useState(characters[0])
   return (
     <div className="App">
       <h1>Fighters</h1>
       <div className="fighters-grid">
         {characters.map((element, i) => {
-          return (<Fighter name={element.name} color={element.color} setVisible={setVisible}/>)
+          return (<Fighter name={element.name} color={element.color} setVisible={setVisible} setSelectedCharacter={setSelectedCharacter}/>)
         })}
       </div>
-    <FighterScreen isVisible={visible} setVisible={setVisible}/>
+    <FighterScreen isVisible={visible} setVisible={setVisible} selectedCharacter={selectedCharacter}/>
     </div>
   );
 }

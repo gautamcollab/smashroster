c63203442c864453e63c96448b4588dc6e7975f3
Gautam Sinha <gautam.sinha@live.com>
2022-04-06 12:00:41 -0400

    added charimages to sidenav

--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -1,15 +1,14 @@
-
-
 const FighterScreen = ({isVisible, setVisible, selectedCharacter}) => {
-    return(
-        <div onClick={() => {return (null)}} 
-        className="fighter-screen" style={{display: isVisible ? "block" : "none"}}> 
-        <span className="name-bg">{selectedCharacter.name}</span>
-        <h2 className="name-fg">{selectedCharacter.name}</h2>
-        <img className="char-portrait" src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedCharacter.name.toLowerCase()}/main.png`}/> 
-        {/* <p className="fighter-desc"> lorem ipsum</p> */}
-      </div>
-    )
+  return(
+    <div onClick={() => { return null }} className="fighter-screen" style={{display: isVisible ? "block" : "none"}}>
+      <span className="name-bg">{selectedCharacter.name}</span>
+      <h2 className="name-fg">{selectedCharacter.name}</h2>
+      <img className="char-portrait" src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedCharacter.name.toLowerCase()}/main.png`} />
+      {/* <p className="fighter-desc">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, voluptatem pariatur. Quos quasi repudiandae, maxime reiciendis labore cupiditate, eligendi similique ratione, dolor est excepturi recusandae voluptas odio ipsam reprehenderit debitis!
+       </p> */}
+    </div>
+  )
 }
 
 export default FighterScreen;
